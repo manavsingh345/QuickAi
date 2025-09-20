8d8acb0ae6fc11b915084368e5c021669e1ce0be
Manav Singh <manavchaudhary060705@gmail.com>
2025-09-20 09:40:48 +0530

    m

--- Frontend/src/Sidebar.tsx
+++ Frontend/src/Sidebar.tsx
@@ -1,5 +1,5 @@
 import "./Sidebar.css"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { MyContext } from "./Context"
 import {v1 as uuidv1} from "uuid";
 
@@ -10,6 +10,8 @@ interface Thread {
 export default function Sidebar(){
 
     const {allThreads,setAllThreads,currThreadId,setPrompt,setnewChat,setReply,setcurrThreadId,setprevChats} = useContext(MyContext);
+    const [CardOpen,SetCardOpen]=useState(false);
+
     const getAllThreads = async () => {
         try{
             const response=await fetch("http://localhost:8000/api/thread");
@@ -56,9 +58,12 @@ export default function Sidebar(){
             console.log(e);
         }
     }
+    const Card= async ()=>{
+        SetCardOpen(true);
+    }
     return(
-        <section className="Sidebar flex flex-col justify-between h-screen" onClick={NewChat}>
-            <button className="flex justify-between items-center cursor-pointer">
+        <section className="Sidebar flex flex-col justify-between h-screen" >
+            <button className="flex justify-between items-center cursor-pointer" onClick={NewChat}>
                 <img src="src\assets\blacklogo.png" alt="chatgptLogo" className="logo"/>
                 <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
@@ -75,10 +80,22 @@ export default function Sidebar(){
                 }
             </ul>
 
-            <div className="sign bottom-0 fixed flex pt-4 pl-2">
+            <div className="sign bottom-0 fixed flex pt-4 pl-2 cursor-pointer" onClick={Card}>
                 <span className="userIcon h-6 w-6 rounded-full flex justify-center items-center cursor-pointer"><i className="fa-solid fa-user"></i></span>
-                <p className="pl-2">Apna College</p>
+                <p className="pl-2">Manav Singh</p>
             </div>
+
+           {CardOpen && <div className="h-90 w-60 bg-gray-700 m-4 rounded-lg pr-4 mr-4 mb-15 bottom-0 left-0" >
+           <span className="flex justify-center p-2 text-sm "><i className="fa-solid fa-user mr-2 pt-1"></i>manavsingh321@gmail.com</span>
+           <div className="m-2 p-2">
+                <p>Apperance</p>
+                <p>Settings</p>
+                
+           </div>
+           <button>Logout</button>
+        </div>}
         </section>
+        
+        
     )
 };
